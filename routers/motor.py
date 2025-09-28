56cee310027521f51910ac1e9132e0ea682c6501
Andrew <92193378+Andrew7-7@users.noreply.github.com>
2025-09-28 13:27:19 +0700

    fix: Request Json

--- routers/motor.py
+++ routers/motor.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from fastapi.openapi.utils import status_code_ranges
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 from publish import motor
 import globals
 
@@ -10,7 +11,11 @@ route = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+class Item(BaseModel):
+    description: str
+    enable: bool
+
 @route.post("/drive")
-async def drive_motor(direction: str, enable: bool):
-    result = await motor.publish_motor(direction, enable)
+async def drive_motor(item: Item):
+    result = await motor.publish_motor(item.description, item.enable)
     return JSONResponse(content=result, status_code=200)
