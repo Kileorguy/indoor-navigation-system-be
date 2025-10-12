8016faaf2464c75740708a120fc0b0e577ed74a3
hendrahartanto <hendrazhuo10@gmail.com>
2025-10-12 12:49:40 +0700

    feat: add ble_sample_data res logger

--- subscribers/ble_sample_data.py
+++ subscribers/ble_sample_data.py
@@ -13,6 +13,7 @@ endpoint untuk calibrate (untuk sekarang belum digunakan)
 @fast_mqtt.subscribe("things/calibrate", qos=0)
 async def insert_ble_sample_data(client: MQTTClient, topic: str, payload: bytes, qos: int, properties: Any):
     payload = json.loads(payload.decode())
-    await service.insert_ble_sample_data(payload)
+    res = await service.insert_ble_sample_data(payload)
 
+    logger.info(res)
     return
